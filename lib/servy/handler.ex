8cb32745d06e9eb4a2c4833ed838d2ccbbdd1d83
Jose Naranjo <joselnaranjo@gmail.com>
2017-10-01 23:40:36 -0500

    07 params status

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,7 +1,7 @@
 defmodule Servy.Handler do
   def handle(request) do
     request 
-    |> parse 
+    |> parse
     |> log
     |> route 
     |> format_response
@@ -14,25 +14,37 @@ defmodule Servy.Handler do
       |> String.split("\n") 
       |> List.first 
       |> String.split(" ")
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method, 
+       path: path, 
+       resp_body: "",
+       status: nil
+     }
   end
 
   def route(conv)  do
     route(conv, conv.method, conv.path)
   end
-
+  
   def route(conv, "GET", "/wildthings")  do
-    Map.put(conv, :resp_body, "Bears, Lions, Tigers")
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  end
+  
+  def route(conv, "GET", "/bears")  do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(conv, "GET", "/bears")  do
-    Map.put(conv, :resp_body, "Teddy, Smokey, Paddington")
+  def route(conv, "GET", "/bears/" <> id)  do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+  
+  def route(conv, _method, path)  do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
   def format_response(conv) do
     # TODO: Use values in the map to create an HTTP response string:
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -40,6 +52,17 @@ defmodule Servy.Handler do
     """
   end
 
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+ 
 end
   
 request = """
@@ -67,6 +90,18 @@ response = Servy.Handler.handle(request)
 IO.puts response
 
 request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
 GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
@@ -74,6 +109,6 @@ Accept: */*
 
 """
 
-# response = Servy.Handler.handle(request)
+response = Servy.Handler.handle(request)
 
-# IO.puts response
+IO.puts response
