48b3173659a51471af3764c219d039b5387e4a1f
Jose Naranjo <joselnaranjo@gmail.com>
2017-11-07 19:26:26 -0500

    Include the map function

--- lib/recurse.ex
+++ lib/recurse.ex
@@ -10,7 +10,15 @@ defmodule Recurse do
   end
 
   def triple([], list), do: list
+
+  def my_map([head | tail], func) do
+    [func.(head) | my_map(tail, func)]
+  end
+
+  def my_map([], func), do: []
 end
 
 IO.puts "Sum: #{Recurse.sum([1, 2, 3, 4, 5], 0)}"
 IO.puts "Triple: " <> inspect Recurse.triple([1, 2, 3, 4, 5], [])
+IO.puts "Map: " <> inspect Recurse.my_map([1, 2, 3, 4, 5], &(&1 * 2))
+IO.puts "Map: " <> inspect Recurse.my_map([1, 2, 3, 4, 5], &(&1 * 5))
